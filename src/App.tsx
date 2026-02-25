4b7bb7f634dbec6907311d6f609db3382570a0cc
jeyghosh <adjainojeremi@gmail.com>
2026-02-25 07:28:26 +0500

    Fix GitHub Pages by using root paths and let GitHub Pages handle subdirectory

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/futur-explore">
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/login" element={<Login />} />
